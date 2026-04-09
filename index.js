e173a59b92642cae2830de19af58ee87b89b6cbe
salim-sebil <salim.sebil@student.moringaschool.com>
2026-04-10 01:04:03 +0300

    lab changes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+//1 global scope
+
+const burgers = ["Hamburger", "Cheeseburger"];
+
+let featuredDrink = "Strawberry Milkshake";
+
+//add burger function
+
+function addBurger(){
+    const newBurger = "Flatburger";
+    burgers.push("newBurger");
+}
+
+//block scope
+
+if(true){
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push("anotherNewBurger")
+}
+
+//change featured drink function
+function chageFeaturedDrink(featuredDrink){
+    let featuredDrink = "The JavaShake";
+}
+
