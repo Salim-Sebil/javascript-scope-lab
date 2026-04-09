e5e7bfe13942de9c376e702fa02e28c71dece20f
salim-sebil <salim.sebil@student.moringaschool.com>
2026-04-10 01:40:12 +0300

    changes

--- index.js
+++ index.js
@@ -5,26 +5,24 @@
 const burgers = ["Hamburger", "Cheeseburger"];
 
 let featuredDrink = "Strawberry Milkshake"
-
 //add burger function
 
 function addBurger(){
     const newBurger = "Flatburger";
     burgers.push(newBurger)
 }
-addBurger()
+
 
 //block scope
 
-if(1 < 3){
+if(true){
     const anotherNewBurger = 'Maple Bacon Burger';
     burgers.push(anotherNewBurger)
 }
 
 //change featured drink function
 function changeFeaturedDrink(){
-    let featuredDrink = 'The JavaShake'
+    featuredDrink = 'The JavaShake'
 }
-changeFeaturedDrink()
 
 console.log(burgers);
